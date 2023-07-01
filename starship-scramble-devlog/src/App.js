455281ed30a2082bb7e850e202d795c61eab9899
KultrasHub <Kultras1808@gmail.com>
2023-07-01 22:35:57 +0700

    change default link

--- starship-scramble-devlog/src/App.js
+++ starship-scramble-devlog/src/App.js
@@ -10,8 +10,7 @@ function App() {
     <Router>
     <Navbar />
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/Starship-Scramble" element={<Home />} />
+      <Route path="/Starship-Scramble-devlog" element={<Home />} />
     </Routes>
     </Router>
   </>
