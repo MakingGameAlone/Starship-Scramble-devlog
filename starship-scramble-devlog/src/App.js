1fd47bb7b0908d094bc144d6bd9d01587b8b7e9a
KultrasHub <Kultras1808@gmail.com>
2023-07-02 23:46:38 +0700

    Modules and weapon

--- starship-scramble-devlog/src/App.js
+++ starship-scramble-devlog/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './Component/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Page/Home';
+import Feature from './Page/Feature';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <Navbar />
     <Routes>
       <Route path="/Starship-Scramble-devlog" element={<Home />} />
+      <Route path="/feature" element={<Feature />} />
     </Routes>
     </Router>
   </>
